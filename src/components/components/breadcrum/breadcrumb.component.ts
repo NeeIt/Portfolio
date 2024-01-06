f1d8bb0c7c09037e49bf410a78a1fd352837d990
NeeProgram <VladViklund@gmail.com>
2024-01-06 22:42:23 +0900

    fix(breadcrum): tab indexes

--- src/components/components/breadcrum/breadcrumb.component.ts
+++ src/components/components/breadcrum/breadcrumb.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {DEFAULT_LANGUAGE} from "@constants/base/language.const";
 import {MyTranslateService} from "@services/translate.service";
+import {TabIndexService} from "@services/tab-index.service";
 
 @Component({
   selector: 'app-breadcrumb',
@@ -10,9 +11,13 @@ import {MyTranslateService} from "@services/translate.service";
 export class BreadCrumbComponent {
   readonly defaultLang = DEFAULT_LANGUAGE;
   readonly currentLang$ = this.translateService.currentLang$;
+  readonly tabIndexes$ = this.tabIndexService.tabIndexValues$;
 
   @Input() breadcrumbs: { title: string, url?: string }[] = [];
 
-  constructor(private translateService: MyTranslateService) {
+  constructor(
+    private translateService: MyTranslateService,
+    private tabIndexService: TabIndexService
+  ) {
   }
 }
